81d9d640ebff3dfffd53459a1bf39bcac06e7afa
jgadia <jgadia@uccs.edu>
2022-04-28 03:59:05 +0000

    beginning home and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  root "projects#index"
+  root "home#index"
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
